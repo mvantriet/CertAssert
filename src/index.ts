f8df5f983818983819667b0d3dd044a7e752d41c
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-28 20:43:36 +0000

    Create instance of CasServer and define the initial route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
+import express from 'express'
+import * as core from 'express-serve-static-core';
+import { ICasServer } from './server/interfaces/ICasServer';
+import { CasServer } from './server/components/CasServer';
+
 export class CertAssert {
 
-    hello(): void {
-        console.log('CertAssert');
+    private app: core.Express;
+    private server: ICasServer;
+
+    constructor() {
+        this.app = express();
+        this.server = new CasServer('./tests/integration/gen/cert/server/certassertServerCert.key', './tests/integration/gen/cert/server/certassertServerCert.pem', ['./tests/integration/gen/cert/ca/CertAssertLocalCA.pem'],
+                                        this.app, 8443, 8080);
     }
 
+    init(): void {
+        this.initRoutes();
+        this.server.init();
+    }
+
+    private initRoutes(): void {
+        this.app.get( "/api/auth", ( req: any, res: any ) => {
+            return res.status(200).send({ result: req.client.authorized, token: 'test' });
+        } );
+    }
 } 
