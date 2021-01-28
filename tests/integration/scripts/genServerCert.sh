70d50e963ef549872a249613b92ee75361c7345e
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-28 20:42:37 +0000

    Helper script for integration testing to bootstrap the server with a self-signed local server cert

---
+++ tests/integration/scripts/genServerCert.sh
@@ -0,0 +1,11 @@
+CWD=$(dirname "$0")
+GEN_DIR="$CWD/../gen/cert/server"
+echo "Making gen directory if it does not exist:"
+mkdir -p $GEN_DIR
+GEN_DIR_ABS="$(cd "$(dirname "$GEN_DIR")"; pwd)/$(basename "$GEN_DIR")"
+echo "Output dir: $GEN_DIR_ABS"
+
+echo "Generating self signed server certificate for testing locally"
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+    -subj "/C=NL/ST=OVR/O=CERTASSERT/CN=certassert" \
+    -keyout $GEN_DIR/certassertServerCert.key  -out $GEN_DIR/certassertServerCert.pem
