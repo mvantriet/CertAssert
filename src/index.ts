c0d32edd90ecc43102935da32f2b51a92f52ced6
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-03 14:10:09 +0000

    Define config type, use logger and router component

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,46 @@
+// Imports
 import express from 'express'
 import * as core from 'express-serve-static-core';
 import { ICasServer } from './server/interfaces/ICasServer';
 import { CasServer } from './server/components/CasServer';
+import { ICasLogger, CasLogLevel } from './logging/interfaces/ICasLogger';
+import { CasLogger } from './logging/components/CasLogger';
+import { ICasRouter } from './routing/interfaces/ICasRouter';
+import { CasRouter } from './routing/components/CasRouter';
+// Import type definitions
+import './dataAdaptation/networking/types/CasNetworkingTypes';
+
+// Exports for external config
+export { CasLogLevel } from './logging/interfaces/ICasLogger';
+
+export type CertAssertConfig = {
+    serverCertificatePath: string;
+    serverCertificateKeyPath: string;
+    acceptedCAs: Array<string>;
+    securePort: number,
+    httpRedirectPort: number,
+    logLevel: CasLogLevel
+}
 
 export class CertAssert {
 
     private app: core.Express;
     private server: ICasServer;
+    private logger: ICasLogger;
+    private router: ICasRouter;
 
-    constructor() {
+    constructor(config: CertAssertConfig) {
         this.app = express();
-        this.server = new CasServer('./test/integration/gen/cert/server/certassertServerCert.key', './test/integration/gen/cert/server/certassertServerCert.pem', ['./test/integration/gen/cert/ca/CertAssertLocalCA.pem'],
-                                        this.app, 8443, 8080);
+        this.logger = new CasLogger(config.logLevel);
+        this.router = new CasRouter(this.logger);
+        this.server = new CasServer(config.serverCertificateKeyPath, config.serverCertificatePath, config.acceptedCAs,
+                                        this.app, config.securePort, config.httpRedirectPort, this.logger);
     }
 
     init(): void {
-        this.initRoutes();
+        this.logger.log('Initiating CertAssert');
+        this.app.use(express.json());
+        this.app.use('/api', this.router.toExpressRouter());
         this.server.init();
-    }
-
-    private initRoutes(): void {
-        this.app.get( "/api/auth", ( req: any, res: any ) => {
-            return res.status(200).send({ result: req.client.authorized, token: 'test' });
-        } );
     }
 } 
