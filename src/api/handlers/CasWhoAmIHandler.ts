47f568112348887c8675b3f9538ed6213bddb635
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-09 21:41:51 +0000

    New handler for /whoami path

---
+++ src/api/handlers/CasWhoAmIHandler.ts
@@ -0,0 +1,20 @@
+import { ICasApiHandler } from "../interfaces/ICasApiHandler";
+import {Request, Response} from 'express';
+import { CasHandler } from "../../common/CasHandler";
+import { ICasLogger } from "../../logging/interfaces/ICasLogger";
+import { ICasDb } from '../../db/interfaces/ICasDb';
+
+export class CasWhoAmIHandler extends CasHandler implements ICasApiHandler {
+    
+    constructor(db: ICasDb, logger: ICasLogger) {
+        super(db, logger);
+    }
+
+    public handle(req: Request, resp: Response): void {
+        if (req.client.authorized) {
+            resp.status(200).send({result: req.client.authorized});
+        } else {
+            resp.status(403).send({authorized: req.client.authorized, cert: {}});
+        }
+    }
+}
