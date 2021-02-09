8cda032db5de8829a5083318d6c510e157bd7268
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-09 21:39:01 +0000

    Simple in mem implementation of db interface using a map

---
+++ src/db/components/CasDbInMem.ts
@@ -0,0 +1,31 @@
+import { ICasDb } from '../interfaces/ICasDb';
+import {CasCert} from '../../model/CasCert';
+import { CasComponent } from '../../common/CasComponent';
+import { ICasLogger } from '../../logging/interfaces/ICasLogger';
+
+export class CasDbInMem extends CasComponent implements ICasDb {
+
+    private certs:Map<string, CasCert.Cert>;
+
+    constructor(logger: ICasLogger) {
+        super(logger);
+        this.certs = new Map<string, CasCert.Cert>();
+    }
+
+    putCert(cert: CasCert.Cert): void {
+        this.certs.set(cert.sha256DigestHex, cert);
+    }
+
+    getCert(shaDigest: string): CasCert.Cert | null {
+        return this.hasCert(shaDigest) ? this.certs.get(shaDigest) : null;
+    }
+
+    hasCert(shaDigest: string): boolean {
+        return this.certs.has(shaDigest);
+    }
+
+    knownCerts(): Array<string> {
+        return Array.from(this.certs.keys());
+    }
+    
+}
