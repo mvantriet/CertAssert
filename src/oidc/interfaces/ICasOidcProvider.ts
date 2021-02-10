c3a242d710fb58df8b169349eab780d9c985ff74
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-10 23:23:49 +0000

    Interface definition for OIDC provider that can be used for express apps

---
+++ src/oidc/interfaces/ICasOidcProvider.ts
@@ -0,0 +1,6 @@
+import * as http from 'http';
+import * as http2 from 'http2';
+
+export interface ICasOidcProvider {
+    getCallback(): (req: http.IncomingMessage | http2.Http2ServerRequest, res: http.ServerResponse | http2.Http2ServerResponse) => void;
+}
