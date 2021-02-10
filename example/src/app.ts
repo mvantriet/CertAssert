627a85993ecde51ae9fdfa47e6485014ada88fb9
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-10 23:25:42 +0000

    Define issuer field

--- example/src/app.ts
+++ example/src/app.ts
@@ -4,6 +4,7 @@ const config:CertAssertConfig = {
     acceptedCAs: ['./test/integration/gen/cert/ca/CertAssertLocalCA.pem'],
     serverCertificatePath: './test/integration/gen/cert/server/certassertServerCert.pem',
     serverCertificateKeyPath: './test/integration/gen/cert/server/certassertServerCert.key',
+    oidcIssuer: 'http://localhost',
     httpRedirectPort: 8080,
     securePort: 8443,
     logLevel: CasLogLevel.DEBUG
