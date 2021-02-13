58d6a462f8299ad872788d8615eb8445bb37c4e7
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-13 23:35:01 +0000

    Instantiate interactions router

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import express from 'express'
 import * as core from 'express-serve-static-core';
 import { Provider } from 'oidc-provider';
 import helmet from 'helmet';
+import compression from "compression";
 const cors = require('cors');
 import { ICasServer } from './server/interfaces/ICasServer';
 import { CasServer } from './server/components/CasServer';
 import { ICasLogger, CasLogLevel } from './logging/interfaces/ICasLogger';
 import { CasLogger } from './logging/components/CasLogger';
 import { ICasRouter } from './routing/interfaces/ICasRouter';
-import { CasRouter } from './routing/components/CasRouter';
+import { CasApiRouter } from './api/components/CasApiRouter';
+import { CasInteractionsRouter } from './interactions/components/CasInteractionsRouter';
 import { ICasOidcProvider } from './oidc/interfaces/ICasOidcProvider';
 import { CasOidcMtlsProvider } from './oidc/components/CasOidcMtlsProvider';
 // Import type definitions
@@ -35,15 +37,17 @@ export class CertAssert {
     private server: ICasServer;
     private logger: ICasLogger;
     private router: ICasRouter;
+    private interactions: CasInteractionsRouter;
     private oidc: ICasOidcProvider;
     private db: ICasDb;
 
     constructor(config: CertAssertConfig) {
         this.app = express();
-        this.oidc = new CasOidcMtlsProvider(`http://localhost:${3000}`, './test/integration/gen/cert/ca/CertAssertLocalCA.pem', './test/integration/gen/cert/ca/CertAssertLocalCA.key');
+        this.oidc = new CasOidcMtlsProvider(`https://localhost:${config.securePort}`, './test/integration/gen/cert/ca/CertAssertLocalCA.pem', './test/integration/gen/cert/ca/CertAssertLocalCA.key');
         this.logger = new CasLogger(config.logLevel);
         this.db = new CasDbInMem(this.logger);
-        this.router = new CasRouter(this.db, this.logger);
+        this.router = new CasApiRouter(this.db, this.logger);
+        this.interactions = new CasInteractionsRouter(this.db, this.logger);
         this.server = new CasServer(config.serverCertificateKeyPath, config.serverCertificatePath, config.acceptedCAs,
                                         this.app, config.securePort, config.httpRedirectPort, this.logger);
     }
@@ -53,7 +57,11 @@ export class CertAssert {
         this.app.use(express.json());
         this.app.use(cors());
         this.app.use(helmet());
-        this.app.use('/api', this.router.toExpressRouter());
+        this.app.use(compression());
+        this.app.use(express.static("public"));
+        this.app.use('/api', this.router.toRouter());
+        this.app.use("/interactions", this.interactions.toRouter());
+        this.app.use(this.oidc.getCallback());
         this.server.init();
     }
 } 
