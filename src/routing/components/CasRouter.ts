37bc25d85b2e01241cce04f968fe323a1513facb
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-13 23:31:25 +0000

    Migrate API routes into dedicated module, make casrouter a reusable router component

--- src/routing/components/CasRouter.ts
+++ src/routing/components/CasRouter.ts
@@ -1,42 +1,39 @@
 import { Router } from 'express';
 import { ICasRouter } from '../interfaces/ICasRouter';
 import { ICasLogger, CasLogLevel } from '../../logging/interfaces/ICasLogger';
-import { ICasApiHandler } from '../../api/interfaces/ICasApiHandler';
+import { ICasRouteHandle } from '../interfaces/ICasApiHandler';
 import { CasHandler } from '../../common/CasHandler';
-import { CasAuthHandler } from '../../api/handlers/CasAuthHandler';
-import { CasWhoAmIHandler } from '../../api/handlers/CasWhoAmIHandler';
-import { CasListHandler } from '../../api/handlers/CasListHandler';
 import { ICasDb } from '../../db/interfaces/ICasDb';
 
-enum HTTP_METHOD {
+export enum HTTP_METHOD {
     GET,
     POST
 }
 
-type EndPointConfig = {
+export type EndPointConfig = {
     apipath: string,
     httpMethod: HTTP_METHOD,
-    handler: ICasApiHandler
+    handlers: Array<ICasRouteHandle>,
 }
 
-export class CasRouter extends CasHandler implements ICasRouter {
+export abstract class CasRouter extends CasHandler implements ICasRouter {
 
-    private routeDefinitions: Array<EndPointConfig>;
+    protected routeDefinitions: Array<EndPointConfig>;
 
     constructor(db: ICasDb, logger: ICasLogger) {
         super(db, logger);
         this.routeDefinitions = this.getRouteDefinitions();
     }
 
-    public toExpressRouter(): Router  {
+    public toRouter(): Router {
         const out: Router = Router();
         this.routeDefinitions.forEach((routeDefinition: EndPointConfig) => {
             switch (routeDefinition.httpMethod) {
                 case HTTP_METHOD.GET:
-                    out.get(routeDefinition.apipath, routeDefinition.handler.handle);
+                    out.get(routeDefinition.apipath, routeDefinition.handlers);
                     break;
                 case HTTP_METHOD.POST:
-                    out.post(routeDefinition.apipath, routeDefinition.handler.handle);
+                    out.post(routeDefinition.apipath, routeDefinition.handlers);
                     break;
                 default:
                     this.logger.log('Unsupported HTTP method for route configuration', CasLogLevel.CRITICAL);
@@ -47,23 +44,5 @@ export class CasRouter extends CasHandler implements ICasRouter {
         return out;
     }
 
-    private getRouteDefinitions(): Array<EndPointConfig> {
-        return [
-        {
-            apipath: '/auth',
-            httpMethod: HTTP_METHOD.GET,
-            handler: new CasAuthHandler(this.db, this.logger)
-        },
-        {
-            apipath: '/whoami',
-            httpMethod: HTTP_METHOD.GET,
-            handler: new CasWhoAmIHandler(this.db, this.logger)
-        },
-        {
-            apipath: '/list',
-            httpMethod: HTTP_METHOD.GET,
-            handler: new CasListHandler(this.db, this.logger)
-        }
-        ]
-    }
+    protected abstract getRouteDefinitions(): Array<EndPointConfig>;
 }
