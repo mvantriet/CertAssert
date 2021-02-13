f7c1f946a07f74daa86ccf0303a2a981899157d2
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-13 23:34:24 +0000

    Style definition for login view

---
+++ src/interactions/views/login/CasLoginView.styled.ts
@@ -0,0 +1,14 @@
+import * as styled from 'styled-components'
+
+export type CasLoginViewStyledProps = {
+}
+
+const CasLoginViewStyled = styled.default.div<CasLoginViewStyledProps>`
+    color: green;
+    background-color: black;
+
+    h1 {
+        color: green;
+    };
+`
+export default CasLoginViewStyled;
