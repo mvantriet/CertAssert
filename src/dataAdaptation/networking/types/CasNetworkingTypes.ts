3bcc5934e282d369a09ccd807c4fa248909e7d1c
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-14 22:26:10 +0000

    Extend req type with cas field containing input adaptations

--- src/dataAdaptation/networking/types/CasNetworkingTypes.ts
+++ src/dataAdaptation/networking/types/CasNetworkingTypes.ts
@@ -1,10 +1,16 @@
+import { CasCert } from "../../../model/CasCert";
 // Extend Express namespace
 declare global {
     namespace Express {
         interface Request {
-            client: RequestClient;
+            client: RequestClient,
+            cas: RequestCasExtensions
         }
     }
+}
+
+export type RequestCasExtensions = {
+    clientCertificate?: CasCert.ClientCert
 }
 
 export type RequestClient = {
