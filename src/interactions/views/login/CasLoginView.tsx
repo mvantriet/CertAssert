bbbf7a502f5a23c523c6cc3cb2e1b98b07fadcb4
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-14 22:35:52 +0000

    Define input properties to render the login view: authorised and the certificate details

--- src/interactions/views/login/CasLoginView.tsx
+++ src/interactions/views/login/CasLoginView.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import CasLoginViewStyled from "./CasLoginView.styled";
+import { RequestCasExtensions } from "../../../dataAdaptation/networking/types/CasNetworkingTypes";
 
 interface CasLoginViewState {
 }
   
 export type CasLoginViewProps = {
-    title: string;
+    title: string,
+    authorised: boolean,
+    casExtensions: RequestCasExtensions
 }
 
 export class CasLoginView extends React.Component<CasLoginViewProps, CasLoginViewState> {
@@ -16,9 +19,9 @@ export class CasLoginView extends React.Component<CasLoginViewProps, CasLoginVie
 
   render() {
     return (
-      <CasLoginViewStyled>
-        <h1>{this.props.title}</h1>
+      <CasLoginViewStyled authorised={this.props.authorised}>
+        <h1>{this.props.authorised ? 'Authorised' : 'Unauthorised'}</h1>
       </CasLoginViewStyled>
-    );
-  }
+    );  
+}
 }
