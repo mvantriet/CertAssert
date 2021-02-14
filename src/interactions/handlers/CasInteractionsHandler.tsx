674240d50c28878f8bc5fa361c0abd454bbcc3ea
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-14 22:35:11 +0000

    Use the hbs template and helper to render the login view

--- src/interactions/handlers/CasInteractionsHandler.tsx
+++ src/interactions/handlers/CasInteractionsHandler.tsx
@@ -1,39 +1,27 @@
 import {Request, Response} from 'express';
-import { renderToString } from "react-dom/server";
 import React from "react";
-import hbs from "handlebars";
 import { CasHandler } from "../../common/CasHandler";
 import { ICasLogger } from "../../logging/interfaces/ICasLogger";
 import { ICasDb } from '../../db/interfaces/ICasDb';
 import { ICasApiHandler } from "../../routing/interfaces/ICasApiHandler";
 import { CasLoginView } from "../views/login/CasLoginView";
-import { ServerStyleSheet } from 'styled-components';
+import { Ssr } from "../views/ssr/CasSsrHelper";
 
 export class CasInteractionsHandler extends CasHandler implements ICasApiHandler {
     constructor(db: ICasDb, logger: ICasLogger) {
         super(db, logger);
     }
 
-    public handle(_req: Request, resp: Response): void {
-        const template = `
-        <html>
-        <head>
-        {{{styles}}}
-        <title>CertAssert</title>
-        </head>
-        <body>
-        <div id="ssrContainer">{{{ssrContainer}}}</div>
-        </body>
-        </html>
-        `;
-        const hbsTemplate = hbs.compile(template);
-        const css = new ServerStyleSheet();
-    
-        // Create the markup from the React application
-        const markup = renderToString(
-            css.collectStyles(<CasLoginView title={'Login'}/>)
+    public handle(req: Request, resp: Response): void {
+        console.log(req.client.authorized)
+        console.log(req.cas)
+        
+        resp.send(
+            Ssr('Login',<CasLoginView 
+                            title={'Login' }
+                            authorised={req.client.authorized}
+                            casExtensions={req.cas}
+                        />)
         );
-        const out = hbsTemplate({ styles: css.getStyleTags(), ssrContainer: markup });
-        resp.send(out);
     }
 }
