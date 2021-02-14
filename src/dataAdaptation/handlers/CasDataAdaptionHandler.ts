1bf10902e885b7e72646a24dffa39122703e42ca
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-14 22:25:46 +0000

    Handler adding input adaptations to custom req objects

---
+++ src/dataAdaptation/handlers/CasDataAdaptionHandler.ts
@@ -0,0 +1,28 @@
+import {Request, Response, NextFunction} from "express";
+import { TLSSocket } from "tls";
+import { CasCertInputAdaptor } from "../../dataAdaptation/certificate/components/CasInputAdaptor";
+import { CasCertificateAdaptation } from "../../dataAdaptation/certificate/common/CasCommonAdaptor";
+import { CasHandler } from "../../common/CasHandler";
+import { ICasLogger } from "../../logging/interfaces/ICasLogger";
+import { ICasDb } from '../../db/interfaces/ICasDb';
+import { ICasApiHandler } from "../../routing/interfaces/ICasApiHandler";
+import { RequestCasExtensions } from "../../dataAdaptation/networking/types/CasNetworkingTypes";
+import { CasCert } from "../../model/CasCert";
+
+export class CasDataAdaptationHandler extends CasHandler implements ICasApiHandler {
+    constructor(db: ICasDb, logger: ICasLogger) {
+        super(db, logger);
+    }
+
+    public handle(req: Request, resp: Response, next: NextFunction): void {
+        const casCertificateAdaptation: RequestCasExtensions = {};
+        const clientCertAdaptation:CasCertificateAdaptation.CertParseResult = CasCertInputAdaptor.fromSocket(
+            ((req.socket) as TLSSocket).getPeerCertificate(true), req.client.authorized);
+        if (!clientCertAdaptation.failureDetails) {
+            // Downcast
+            casCertificateAdaptation.clientCertificate = clientCertAdaptation.cert as CasCert.ClientCert;
+        }
+        req.cas = casCertificateAdaptation;
+        next();
+    }
+}
