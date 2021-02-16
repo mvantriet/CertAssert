d6ee7123b2a6f203dfbfbec373c5407fce49f757
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-16 19:21:00 +0000

    Define explicit init method

--- src/oidc/interfaces/ICasOidcProvider.ts
+++ src/oidc/interfaces/ICasOidcProvider.ts
@@ -2,5 +2,6 @@ import * as http from 'http';
 import * as http2 from 'http2';
 
 export interface ICasOidcProvider {
+    init(): Promise<void>;
     getCallback(): (req: http.IncomingMessage | http2.Http2ServerRequest, res: http.ServerResponse | http2.Http2ServerResponse) => void;
 }
