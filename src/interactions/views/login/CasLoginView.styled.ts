5e72090b56b73edfca8bce54fea69a2c7c771859
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-16 19:23:00 +0000

    Server side rendered signin page based on existing template

--- src/interactions/views/login/CasLoginView.styled.ts
+++ src/interactions/views/login/CasLoginView.styled.ts
@@ -5,8 +5,64 @@ export type CasLoginViewStyledProps = {
 }
 
 const CasLoginViewStyled = styled.default.div<CasLoginViewStyledProps>`
-    h1 {
-        color: ${props => props.authorised ? 'green' : 'red'};
-    };
+
+    font-family: 'PT Sans', sans-serif;
+    font-size: 12pt;
+    color: #3b4351;
+    background-color: white;
+
+    h4 {
+        font-size: 18pt;
+        font-weight: bold;
+    }
+
+    .signin-content {
+        padding-left: 10px;
+        padding-bottom: 20px;
+    }
+
+    .status-label {
+        font-size: 14pt;
+    }
+
+    .action-buttons {
+        margin-top: 40px;
+    }
+
+    .authorization-state {
+        font-size: 14pt;
+        font-weight: bold;
+        color: ${(props) => props.authorised ? 'green' : 'red'};
+    }
+
+    .cert-details {
+        margin-top: 20px;
+        padding-left: 10px;
+        box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1), 0 2px 4px 0 rgba(0, 0, 0, 0.06);
+        border-radius: 5px;
+    }
+
+    #ca-signin-btn {
+        margin-top: 10px;
+    }
+
+    #ca-abort-btn {
+        margin-top: 10px;
+    }
+
+    .horizontal-divider {
+        border-top: .05rem solid #eaeded;
+        height: .1rem;
+        width: 100%;
+    }
+
+    .certificate-type-label {
+        margin-top: 10px;
+        font-size: 14pt;
+        font-weight: bold;
+        color: #5755d9;
+        padding-bottom: 10px;
+    }
+
 `
 export default CasLoginViewStyled;
--- src/interactions/views/login/CasLoginView.tsx
+++ src/interactions/views/login/CasLoginView.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CasLoginViewStyled from "./CasLoginView.styled";
 import { RequestCasExtensions } from "../../../dataAdaptation/networking/types/CasNetworkingTypes";
+import { CasCert } from "../../../model/CasCert";
 
 interface CasLoginViewState {
 }
@@ -8,7 +9,9 @@ interface CasLoginViewState {
 export type CasLoginViewProps = {
     title: string,
     authorised: boolean,
-    casExtensions: RequestCasExtensions
+    casExtensions: RequestCasExtensions,
+    signinPath: string,
+    abortPath: string
 }
 
 export class CasLoginView extends React.Component<CasLoginViewProps, CasLoginViewState> {
@@ -18,10 +21,84 @@ export class CasLoginView extends React.Component<CasLoginViewProps, CasLoginVie
   }
 
   render() {
+
+    // Iterate over DN attributes for subject
+    let certificatesRendered: Array<JSX.Element> = [];
+
+    // Iterate the chain
+    let currentCert: CasCert.Cert = this.props.casExtensions.clientCertificate;
+    let index = 0;
+    while(currentCert) {
+        const certDetails: Array<JSX.Element> = [];
+        certDetails.push(<div className={"certificate-type-label"}>{
+          index === 0 ?
+          "Client Certificate" : (currentCert.issuerCertRef ? 'Intermediate CA' : 'Root CA')
+          }</div>);
+        certDetails.push(<div className={"horizontal-divider"} />);
+        Object.entries(CasCert.DistinguishedNameAttribute).forEach((value: [string, CasCert.DistinguishedNameAttribute]) => {
+          if (currentCert.subject[value[1]]) {
+            certDetails.push(
+            <div className={"columns"}>
+              <div className={"column col-xs-4"}>
+                {value[0]}
+              </div>
+              <div className={"column col-xs-8"}>
+                {currentCert.subject[value[1]]}
+              </div>
+            </div>)
+          }
+        })
+      certificatesRendered.push(<div key={index} className={"cert-details"}>{certDetails}</div>);
+      currentCert = currentCert.issuerCertRef;
+      index++;
+    }
     return (
       <CasLoginViewStyled authorised={this.props.authorised}>
-        <h1>{this.props.authorised ? 'Authorised' : 'Unauthorised'}</h1>
+        <div className="container grid-lg p-centered">
+          <div className="columns">
+          <div className="column col-3" />
+            <div className="column col-6">
+              <h4>{"> CertAssert / Sign-In"}</h4>
+              <div className="signin-content">
+                <div className="columns">
+                    <div className={"column col-2 status-label"}>
+                      Status:
+                    </div>
+                    <div className={"column col-2 authorization-state"}>
+                      {this.props.authorised ? 'Authorised' : 'Unauthorised'}
+                    </div>
+                </div>
+                {certificatesRendered}
+                <div className="action-buttons">
+                    {this.props.authorised ? 
+                      <div className="columns">
+                        <div className="column col-2" />
+                        <div className="column col-5">
+                              <div className="columns">
+                                <div className="column">
+                                  <button id="ca-signin-btn" className="btn btn-primary">Sign In</button>
+                                </div>
+                                <div className="divider-vert" data-content="OR">a</div>
+                                <div className="column">
+                                  <button id="ca-abort-btn" className="btn">Abort</button>
+                                </div>
+                              </div>
+                        </div>
+                      </div>
+                      : 
+                      <div className="columns">
+                        <div className="column col-2"/>
+                        <div className="column col-8">
+                          <button id="ca-abort-btn" className="btn btn-primary">Abort</button>
+                        </div>
+                      </div>}
+                </div>
+              </div>
+            </div>
+        </div>
+          
+        </div>
       </CasLoginViewStyled>
     );  
-}
+  }
 }
