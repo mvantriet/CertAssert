00a91f29531f60c2264d962351f6f2966369f7aa
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-16 19:21:59 +0000

    Helper method to fetch attribute from DN or fallback if it's not defined

--- src/utils/CertificateUtils.ts
+++ src/utils/CertificateUtils.ts
@@ -1,10 +1,18 @@
+import { CasCert } from "../model/CasCert";
+
 export class CertificateUtils {
     
+    static nonExistentDnAttributeValue: string = "";
+
     static normaliseCert(pem: string): string {
         return pem.replace(/(?:-----(?:BEGIN|END) CERTIFICATE-----|\s)/g, '');
     }
 
     static normaliseKey(key: string): string {
         return key.replace(/(?:-----(?:BEGIN|END) ENCRYPTED PRIVATE KEY-----|\s)/g, '');
+    }
+
+    static getDnAttributeValue(dn: CasCert.DistinguishedName, attr: CasCert.DistinguishedNameAttribute, fallback?: string): string {
+        return (dn[attr] ? dn[attr] : (fallback ? fallback : this.nonExistentDnAttributeValue));
     }
 }
