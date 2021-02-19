0180de36223060e34d96435905148bfac04cd26b
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-19 13:59:46 +0000

    Define API constants in dedicated module to facilitate isolated usage from external parties, e.g. front-end pages

---
+++ src/api/constants/CasApiConstants.ts
@@ -0,0 +1,13 @@
+type CasApiConstantsType = {
+    prefix: string,
+    whoamiPath: string,
+    authPath: string,
+    listPath: string
+}
+
+export const CasApiConstants:CasApiConstantsType = {
+    prefix: '/api',
+    whoamiPath: '/whoami',
+    authPath: '/auth',
+    listPath: '/list'
+}
