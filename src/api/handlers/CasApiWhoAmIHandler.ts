c8ad5f7c6bae9030484e6b1e7ca9ee58f25c42c6
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-19 13:42:54 +0000

    Implement whoami handler

--- src/api/handlers/CasApiWhoAmIHandler.ts
+++ src/api/handlers/CasApiWhoAmIHandler.ts
@@ -3,18 +3,28 @@ import {Request, Response} from 'express';
 import { CasHandler } from "../../common/CasHandler";
 import { ICasLogger } from "../../logging/interfaces/ICasLogger";
 import { ICasDb } from '../../db/interfaces/ICasDb';
+import { ClientCert } from '../../model/CasCert';
 
-export class CasWhoAmIHandler extends CasHandler implements ICasApiHandler {
+export type CasApiWhoAmiResponse = {
+    authorised: boolean,
+    cert?: ClientCert
+}
+
+export class CasApiWhoAmIHandler extends CasHandler implements ICasApiHandler {
     
     constructor(db: ICasDb, logger: ICasLogger) {
         super(db, logger);
     }
 
     public handle(req: Request, resp: Response): void {
+        const out: CasApiWhoAmiResponse = {
+            authorised: req.client.authorized
+        };
         if (req.client.authorized) {
-            resp.status(200).send({authorized: req.client.authorized, cert: req.cas.clientCertificate});
+            out.cert = req.cas.clientCertificate;
+            resp.status(200).send(out);
         } else {
-            resp.status(403).send({authorized: false});
+            resp.status(403).send(out);
         }
     }
 }
