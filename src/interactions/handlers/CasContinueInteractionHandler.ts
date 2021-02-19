0230f09636235ce568bf6f0f01ac8dac32ed788e
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-19 14:12:23 +0000

    Initial implementation for the continue interaction

---
+++ src/interactions/handlers/CasContinueInteractionHandler.ts
@@ -0,0 +1,24 @@
+import { ICasApiHandler } from "../../routing/interfaces/ICasApiHandler";
+import {Request, Response} from 'express';
+import { CasHandler } from "../../common/CasHandler";
+import { ICasLogger } from "../../logging/interfaces/ICasLogger";
+import { ICasDb } from '../../db/interfaces/ICasDb';
+import { ICasOidcInteractionsProvider } from "../../oidc/interfaces/ICasOidcInteractionsProvider";
+
+export class CasContinueInteractionHandler extends CasHandler implements ICasApiHandler {
+    
+    private interactionsProvider:ICasOidcInteractionsProvider;
+
+    constructor(db: ICasDb, logger: ICasLogger, interactionsProvider: ICasOidcInteractionsProvider) {
+        super(db, logger);
+        this.interactionsProvider = interactionsProvider;
+    }
+
+    public handle(req: Request, resp: Response): void {
+        if (req.client.authorized) {
+            resp.status(200).send({});
+        } else {
+            resp.status(403).send({});
+        }
+    }
+}
