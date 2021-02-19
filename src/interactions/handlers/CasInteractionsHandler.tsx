ece1bbb72499e79d35ef27bc086483800428b20a
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-19 13:49:07 +0000

    Implement interactions handler for login and consent paths to reroute to static user pages

--- src/interactions/handlers/CasInteractionsHandler.tsx
+++ src/interactions/handlers/CasInteractionsHandler.tsx
@@ -1,32 +1,46 @@
 import {Request, Response} from 'express';
-import React from "react";
 import { CasHandler } from "../../common/CasHandler";
 import { ICasLogger } from "../../logging/interfaces/ICasLogger";
 import { ICasDb } from '../../db/interfaces/ICasDb';
 import { ICasApiHandler } from "../../routing/interfaces/ICasApiHandler";
-import { CasLoginView } from "../views/login/CasLoginView";
-import { Ssr } from "../views/ssr/CasSsrHelper";
+import { ICasOidcInteractionsProvider, CasOidcInteractionDetails } from '../../oidc/interfaces/ICasOidcInteractionsProvider';
+import { InteractionsStaticConstants } from '../static/src/constants/InteractionsStaticConstants';
+import { PathUtils } from '../../utils/PathUtils';
+
+enum OIDC_INTERACTION {
+    LOGIN='login',
+    CONSENT='consent'
+}
 
 export class CasInteractionsHandler extends CasHandler implements ICasApiHandler {
     
-    private singinPath: string;
-    private abortPath: string;
+    private interactionsProvider: ICasOidcInteractionsProvider;
 
-    constructor(db: ICasDb, logger: ICasLogger, signinPath: string, abortPath: string) {
+    constructor(db: ICasDb, logger: ICasLogger, interactionsProvider: ICasOidcInteractionsProvider) {
         super(db, logger);
-        this.singinPath = signinPath;
-        this.abortPath = abortPath;
+        this.interactionsProvider = interactionsProvider;
     }
 
-    public handle(req: Request, resp: Response): void {
-        resp.send(
-            Ssr('Login',<CasLoginView 
-                            title={'Login' }
-                            authorised={req.client.authorized}
-                            casExtensions={req.cas}
-                            signinPath={this.singinPath}
-                            abortPath={this.abortPath}
-                        />)
-        );
+    public async handle(req: Request, resp: Response): Promise<void> {
+        const interactionDetails:CasOidcInteractionDetails = await this.interactionsProvider.getInteractionDetails(req, resp);
+        if (interactionDetails.prompt.name === OIDC_INTERACTION.LOGIN) {
+            PathUtils.redirectResponse(resp, PathUtils.buildInteractionPath(
+                PathUtils.buildPath(false, InteractionsStaticConstants.signinPath), interactionDetails.uid)
+            );
+        }
+        else if (interactionDetails.prompt.name === OIDC_INTERACTION.CONSENT) {
+            PathUtils.redirectResponse(resp, PathUtils.addQueryParams(PathUtils.buildInteractionPath(
+                PathUtils.buildPath(false, InteractionsStaticConstants.consentPath), interactionDetails.uid),
+                [{
+                    name: 'client',
+                    value: interactionDetails.params.client_id
+                },
+                {
+                    name: 'scopes',
+                    value: interactionDetails.prompt.details.scopes.new.join(',')
+                }
+                ])
+            );
+        }
     }
 }
