a44b6345a67fd5dcf2fae31a56f2aac1ac60ddb3
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-19 13:47:14 +0000

    Use external config for path definitions to configure the routes

--- src/interactions/components/CasInteractionsRouter.ts
+++ src/interactions/components/CasInteractionsRouter.ts
@@ -2,34 +2,64 @@ import {HTTP_METHOD, EndPointConfig, CasRouter} from '../../routing/components/C
 import { ICasDb } from '../../db/interfaces/ICasDb';
 import { ICasLogger } from '../../logging/interfaces/ICasLogger';
 import { CasInteractionsHandler } from '../handlers/CasInteractionsHandler'; 
+import { CasLoginInteractionHandler } from '../handlers/CasLoginInteractionHandler';
+import { CasContinueInteractionHandler } from '../handlers/CasContinueInteractionHandler';
+import { CasConfirmInteractionHandler } from '../handlers/CasConfirmInteractionHandler';
+import { CasAbortInteractionHandler } from '../handlers/CasAbortInteractionHandler';
 import { CasHandler } from '../../common/CasHandler';
+import { ICasOidcInteractionsProvider } from '../../oidc/interfaces/ICasOidcInteractionsProvider';
+import { CasInteractionsConstants } from '../constants/CasInteractionsConstants';
 
 export class CasInteractionsRouter extends CasRouter {
 
-    constructor(db: ICasDb, logger: ICasLogger) {
-        super(db, logger);
+    constructor(db: ICasDb, logger: ICasLogger, interactionsProvider: ICasOidcInteractionsProvider) {
+        super(db, logger, interactionsProvider);
+        this.routeDefinitions = this.getRouteDefinitions();
     }
 
     protected getRouteDefinitions(): Array<EndPointConfig> {
-        const signinPath = '/:uid/login';
-        const abortPath = '/:uid/abort';
         return [
             {
-                apipath: '/:uid',
+                apipath: CasInteractionsConstants.interactionPath,
                 httpMethod: HTTP_METHOD.GET,
                 handlers: [
                     CasHandler.disableCache, 
-                    new CasInteractionsHandler(this.db, this.logger, signinPath, abortPath).getHandle()
+                    new CasInteractionsHandler(this.db, this.logger, this.interactionsProvider).getHandle()
                 ]
             },
             {
-                apipath: signinPath,
+                apipath: CasInteractionsConstants.loginPath,
                 httpMethod: HTTP_METHOD.GET,
                 handlers: [
                     CasHandler.disableCache, 
-                    new CasInteractionsHandler(this.db, this.logger, signinPath, abortPath).getHandle()
+                    new CasLoginInteractionHandler(this.db, this.logger, this.interactionsProvider).getHandle()
+                ]
+            },
+            {
+                apipath: CasInteractionsConstants.confirmPath,
+                httpMethod: HTTP_METHOD.GET,
+                handlers: [
+                    CasHandler.disableCache, 
+                    new CasConfirmInteractionHandler(this.db, this.logger, this.interactionsProvider).getHandle()
+                ]
+            },
+            {
+                apipath: CasInteractionsConstants.continuePath,
+                httpMethod: HTTP_METHOD.GET,
+                handlers: [
+                    CasHandler.disableCache, 
+                    new CasContinueInteractionHandler(this.db, this.logger, this.interactionsProvider).getHandle()
+                ]
+            },
+            {
+                apipath: CasInteractionsConstants.abortPath,
+                httpMethod: HTTP_METHOD.GET,
+                handlers: [
+                    CasHandler.disableCache, 
+                    new CasAbortInteractionHandler(this.db, this.logger, this.interactionsProvider).getHandle()
                 ]
             }
+    
         ];
     }
 }
