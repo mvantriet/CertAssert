5fae6055e2d1c0af52e5756692dcf7e938171382
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-20 23:46:07 +0000

    Add routes for error and logout view in views app

--- src/interactions/static/src/interactions-app/CasInteractionsApp.tsx
+++ src/interactions/static/src/interactions-app/CasInteractionsApp.tsx
@@ -3,7 +3,8 @@ import './CasInteractionsApp.css';
 import { Route, Switch } from "react-router-dom";
 import CasLoginView from "../components/login";
 import CasConsentView from "../components/consent";
-
+import CasLogoutView from "../components/logout";
+import CasErrorView from '../components/error/CasErrorView';
 import { InteractionsStaticConstants} from "../constants/InteractionsStaticConstants";
 
 function CasInteractionsApp() {
@@ -12,6 +13,9 @@ function CasInteractionsApp() {
       <Switch>
         <Route path={InteractionsStaticConstants.signinPath} component={CasLoginView} />
         <Route path={InteractionsStaticConstants.consentPath} component={CasConsentView} />
+        <Route path={InteractionsStaticConstants.logoutPath} component={CasLogoutView} />
+        <Route path={InteractionsStaticConstants.errorPath} component={CasErrorView} status={404}/>
+        <Route path={'/*'} component={CasErrorView} status={404}/>
       </Switch>
     </div>
   );
