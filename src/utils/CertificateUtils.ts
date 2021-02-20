a28e4d9019e5294eab527f17bfb8f6104989954c
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-20 23:38:36 +0000

    Utility function for input adapting a set of certs, and a utility function for checking time validity of certs

--- src/utils/CertificateUtils.ts
+++ src/utils/CertificateUtils.ts
@@ -1,5 +1,8 @@
 import * as CasCert from "../model/CasCert";
 import { CasCertInputAdaptor } from "../dataAdaptation/certificate/components/CasInputAdaptor";
+import { CasCertificateAdaptation } from "../dataAdaptation/certificate/common/CasCommonAdaptor";
+import { Cert } from "../model/CasCert";
+import { readFileSync } from "fs";
 
 export class CertificateUtils {
     
@@ -15,5 +18,17 @@ export class CertificateUtils {
 
     static getDnAttributeValue(dn: CasCert.DistinguishedName, attr: CasCert.DistinguishedNameAttribute, fallback?: string): string {
         return (dn[attr] ? dn[attr] : (fallback ? fallback : this.nonExistentDnAttributeValue));
+    }
+
+    static inputAdaptCertArray(certPaths: Array<string>): Array<Cert> {
+        return certPaths.map((ca: string) => readFileSync(ca))
+        .map((caRaw: Buffer) => CasCertificateAdaptation.Adaptor.fromRaw(caRaw))
+        .filter((caParsed: CasCertificateAdaptation.CertParseResult) => caParsed.failureDetails === undefined)
+        .map((caParsed: CasCertificateAdaptation.CertParseResult) => caParsed.cert);
+    }
+
+    static checkTimeValidity(cert: CasCert.Cert): boolean {
+        const now: number = Date.now();
+        return now >= cert.validityPeriod.notBefore && now <= cert.validityPeriod.notAfter;
     }
 }
