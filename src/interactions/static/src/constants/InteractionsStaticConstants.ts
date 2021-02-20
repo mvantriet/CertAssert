d8559e9bbb2c299b579b600273112cac99eca831
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-20 23:37:26 +0000

    Define constant for logout path

--- src/interactions/static/src/constants/InteractionsStaticConstants.ts
+++ src/interactions/static/src/constants/InteractionsStaticConstants.ts
@@ -1,9 +1,13 @@
 export type InteractionsStaticConstantsType = {
-    signinPath: string
-    consentPath: string
+    signinPath: string,
+    consentPath: string,
+    errorPath: string,
+    logoutPath: string
 }
 
 export const InteractionsStaticConstants: InteractionsStaticConstantsType = {
     signinPath: '/signin/:uid',
-    consentPath: '/consent/:uid'
+    consentPath: '/consent/:uid',
+    errorPath: '/error',
+    logoutPath: '/logout/:uid'
 }
