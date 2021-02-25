f63ea9a0e41a6cca5175dc25b86debf11ec95095
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-25 18:32:24 +0000

    Update interface definition in case cert does not exist

--- src/db/interfaces/ICasDb.ts
+++ src/db/interfaces/ICasDb.ts
@@ -1,8 +1,8 @@
-import {CasCert} from '../../model/CasCert';
+import * as CasCert from '../../model/CasCert';
 
 export interface ICasDb {
     putCert(cert: CasCert.Cert): void;
-    getCert(shaDigest: string): CasCert.Cert | null;
+    getCert(shaDigest: string): CasCert.Cert | undefined;
     hasCert(shaDigest: string): boolean;
     knownCerts(): Array<string>;
 }
