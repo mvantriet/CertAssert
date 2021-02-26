b6301dd91fd19e03160264abe22e2a4c61accc46
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-26 23:54:57 +0000

    Check if already generated before, skip in that case

--- test/integration/scripts/genServerCert.sh
+++ test/integration/scripts/genServerCert.sh
@@ -1,5 +1,9 @@
 CWD=$(dirname "$0")
 GEN_DIR="$CWD/../gen/cert/server"
+if [ -d $GEN_DIR ]; then
+    echo "Server cert already generated before..skipping"
+    exit 0
+fi
 echo "Making gen directory if it does not exist:"
 mkdir -p $GEN_DIR
 GEN_DIR_ABS="$(cd "$(dirname "$GEN_DIR")"; pwd)/$(basename "$GEN_DIR")"
