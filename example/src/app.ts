0d4319585c9a30a100e17075a9694702cebc1ae7
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-03-04 17:37:07 +0000

    Adhere the example app to the new config type, by defining the transparentflows for interactions. Turn transparency off for the example app for all interactions

--- example/src/app.ts
+++ example/src/app.ts
@@ -1,4 +1,6 @@
 import { CertAssert, CertAssertConfig, CasLogLevel } from '../../src/index';
+import fs from 'fs';
+import path from 'path';
 
 const config:CertAssertConfig = {
     acceptedCAs: ['test/integration/gen/cert/ca/CertAssertLocalCA.pem'],
@@ -7,7 +9,14 @@ const config:CertAssertConfig = {
     oidcIssuer: 'http://localhost',
     httpRedirectPort: 8080,
     securePort: 9443,
-    logLevel: CasLogLevel.DEBUG
+    logLevel: CasLogLevel.DEBUG,
+    cookieKeys: ['a393f7c7d23b'],
+    webKeys: [fs.readFileSync(path.resolve(__dirname, '..', '..', 'test', 'integration', 'gen', 'cert', 'signing', 'certassertSigningKey.key')).toString('utf-8')],
+    transparentInteractionFlows: {
+        signinPath: false,
+        errorPath: false,
+        consentPath: false,
+        logoutPath: true
+    }
 }
-
-new CertAssert(config).init();
+new CertAssert(config).init();   
