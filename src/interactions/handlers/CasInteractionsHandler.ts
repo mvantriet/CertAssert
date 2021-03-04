774c48e4074d86741de9564bfca4831a79cad989
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-03-04 17:26:08 +0000

    Add parameters to make the signin and consent interactions transparent, i.e. no user interaction is involved

--- src/interactions/handlers/CasInteractionsHandler.ts
+++ src/interactions/handlers/CasInteractionsHandler.ts
@@ -1,10 +1,10 @@
 import {Request, Response} from 'express';
 import { CasHandler } from "../../common/CasHandler";
-import { ICasLogger } from "../../logging/interfaces/ICasLogger";
+import { ICasLogger, CasLogLevel } from "../../logging/interfaces/ICasLogger";
 import { ICasDb } from '../../db/interfaces/ICasDb';
 import { ICasApiHandler } from "../../routing/interfaces/ICasApiHandler";
 import { ICasOidcInteractionsProvider, CasOidcInteractionDetails } from '../../oidc/interfaces/ICasOidcInteractionsProvider';
-import { InteractionsStaticConstants } from '../static/src/constants/InteractionsStaticConstants';
+import { CasInteractionsConstants } from '../constants/CasInteractionsConstants';
 import { PathUtils } from '../../utils/PathUtils';
 import { ObjUtils } from '../../utils/ObjUtils';
 
@@ -16,10 +16,19 @@ enum OIDC_INTERACTION {
 export class CasInteractionsHandler extends CasHandler implements ICasApiHandler {
     
     private interactionsProvider: ICasOidcInteractionsProvider;
+    private signinPath: string;
+    private consentPath: string;
+    private transparentSigninFlow: boolean;
+    private transparentConsentFlow: boolean;
 
-    constructor(db: ICasDb, logger: ICasLogger, interactionsProvider: ICasOidcInteractionsProvider) {
+    constructor(db: ICasDb, logger: ICasLogger, interactionsProvider: ICasOidcInteractionsProvider,
+            signinPath: string, consentPath: string, transparentSigninFlow: boolean, transparentConsentFlow: boolean) {
         super(db, logger);
         this.interactionsProvider = interactionsProvider;
+        this.signinPath = signinPath;
+        this.consentPath = consentPath;
+        this.transparentSigninFlow = transparentSigninFlow;
+        this.transparentConsentFlow = transparentConsentFlow;
     }
 
     public async handle(req: Request, resp: Response): Promise<void> {
@@ -27,13 +36,19 @@ export class CasInteractionsHandler extends CasHandler implements ICasApiHandler
         if (interaction) {
             const interactionDetails: CasOidcInteractionDetails = interaction as CasOidcInteractionDetails;
             if (interactionDetails.prompt.name === OIDC_INTERACTION.LOGIN) {
-                PathUtils.redirectResponse(resp, PathUtils.buildInteractionPath(
-                    PathUtils.buildPath(false, InteractionsStaticConstants.signinPath), interactionDetails.uid)
-                );
+                let interactionStep: string = PathUtils.buildInteractionPath(
+                    PathUtils.buildPath(false, this.signinPath), interactionDetails.uid);
+                if (this.transparentSigninFlow) {
+                    this.logger.log(`Using transparent interaction flow for login interaction: ${interactionDetails.uid}`, CasLogLevel.DEBUG);
+                    interactionStep = PathUtils.buildInteractionPath(PathUtils.buildPath(false,
+                        CasInteractionsConstants.prefix, CasInteractionsConstants.loginPath), interactionDetails.uid);
+                }
+                this.logger.log(`Redirecting to login: ${interactionStep}`, CasLogLevel.DEBUG);
+                PathUtils.redirectResponse(resp, interactionStep);
             }
             else if (interactionDetails.prompt.name === OIDC_INTERACTION.CONSENT) {
-                PathUtils.redirectResponse(resp, PathUtils.addQueryParams(PathUtils.buildInteractionPath(
-                    PathUtils.buildPath(false, InteractionsStaticConstants.consentPath), interactionDetails.uid),
+                let interactionStep: string = PathUtils.addQueryParams(PathUtils.buildInteractionPath(
+                    PathUtils.buildPath(false, this.consentPath), interactionDetails.uid),
                     [{
                         name: 'client',
                         value: ObjUtils.fetchField<string>(interactionDetails, ['params', 'client_id'], "unknown")
@@ -42,8 +57,14 @@ export class CasInteractionsHandler extends CasHandler implements ICasApiHandler
                         name: 'scopes',
                         value: ObjUtils.fetchField<Array<string>>(interactionDetails.prompt, ['details', 'scopes', 'new'], []).join(',')
                     }
-                    ])
-                );
+                    ]);
+                if (this.transparentConsentFlow) {
+                    this.logger.log(`Using transparent interaction flow for consent interaction: ${interactionDetails.uid}`, CasLogLevel.DEBUG);
+                    interactionStep = PathUtils.buildInteractionPath(PathUtils.buildPath(false,
+                        CasInteractionsConstants.prefix, CasInteractionsConstants.confirmPath), interactionDetails.uid);
+                }
+                this.logger.log(`Redirecting to consent: ${interactionStep}`, CasLogLevel.DEBUG);
+                PathUtils.redirectResponse(resp, interactionStep);
             }               
         }
     }
