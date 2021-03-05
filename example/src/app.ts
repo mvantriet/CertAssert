d5b6455686df21cd4507219582ff336c3226519b
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-03-05 20:55:23 +0000

    Define example client as used in example/app

--- example/src/app.ts
+++ example/src/app.ts
@@ -6,17 +6,27 @@ const config:CertAssertConfig = {
     acceptedCAs: ['test/integration/gen/cert/ca/CertAssertLocalCA.pem'],
     serverCertificatePath: './test/integration/gen/cert/server/certassertServerCert.pem',
     serverCertificateKeyPath: './test/integration/gen/cert/server/certassertServerCert.key',
-    oidcIssuer: 'http://localhost',
+    oidcIssuer: 'https://lvh.me',
     httpRedirectPort: 8080,
     securePort: 9443,
     logLevel: CasLogLevel.DEBUG,
     cookieKeys: ['a393f7c7d23b'],
     webKeys: [fs.readFileSync(path.resolve(__dirname, '..', '..', 'test', 'integration', 'gen', 'cert', 'signing', 'certassertSigningKey.key')).toString('utf-8')],
+    clients: [
+        {
+            client_id: 'cert-assert-example-client-app',
+            grant_types: ['implicit', 'authorization_code'],
+            response_types: ['id_token', 'code'],
+            redirect_uris: ['https://lvh.me:3000'],
+            post_logout_redirect_uris: ['https://lvh.me:3000'],
+            token_endpoint_auth_method: 'none'
+        }
+    ],
     transparentInteractionFlows: {
         signinPath: false,
         errorPath: false,
         consentPath: false,
-        logoutPath: true
+        logoutPath: false
     }
 }
 new CertAssert(config).init();   
